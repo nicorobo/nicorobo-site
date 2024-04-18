c28c71af4aa90ce31483d58bb1a76e9b444d1986
nicorobo <nickroberts404@gmail.com>
2024-04-17 18:07:43 -0700

    began building project cards

---
+++ src/data/projects.ts
@@ -0,0 +1,43 @@
+export const projects = [
+  {
+    title: 'Harmidi',
+    description: 'This is a description of project 1',
+    githubUrl: 'https://github.com/nicorobo/Harmidi',
+    url: 'https://nicorobo.github.io/Harmidi/',
+    stack: ['TypeScript', 'React', 'zustand', 'Joy UI'],
+  },
+  {
+    title: '@react-midi/hooks',
+    description: 'This is a description of project 1',
+    githubUrl: 'https://github.com/nicorobo/Harmidi',
+    url: 'https://nicorobo.github.io/Harmidi/',
+    stack: ['TypeScript', 'React'],
+  },
+  {
+    title: 'daily-cli',
+    description: 'This is a description of project 1',
+    githubUrl: 'https://github.com/nicorobo/daily-cli',
+    stack: ['Python', 'SQLite', 'Typer'],
+  },
+  {
+    title: 'al2js',
+    description: 'This is a description of project 1',
+    githubUrl: 'https://github.com/nicorobo/Harmidi',
+    url: 'https://nicorobo.github.io/Harmidi/',
+    stack: ['TypeScript'],
+  },
+  {
+    title: 'Groovin',
+    description: 'This is a description of project 1',
+    githubUrl: 'https://github.com/nicorobo/Harmidi',
+    url: 'https://nicorobo.github.io/Harmidi/',
+    stack: ['React', 'D3'],
+  },
+  {
+    title: 'Elements',
+    description: 'This is a description of project 1',
+    githubUrl: 'https://github.com/nicorobo/Harmidi',
+    url: 'https://nicorobo.github.io/Harmidi/',
+    stack: ['React'],
+  },
+]
